a9b650a70612fbe305d03868de4e6d42be163418
xJsKx <81259908+xJsKx@users.noreply.github.com>
2022-12-18 15:36:36 +0300

    Update install.sh

--- dukaev-ipv6/scripts/install.sh
+++ dukaev-ipv6/scripts/install.sh
@@ -1,52 +1,43 @@
 #!/bin/sh
-echo "Changing interface name"
-/sbin/ip link set ens224 down
-/sbin/ip link set ens224 name eth0
-/sbin/ip link set eth0 up
-
-echo "Delete old directories if exists"
-rm -rf /usr/local/etc/3proxy/
-rm -rf /home/proxy-installer
-
-
 random() {
-	tr </dev/urandom -dc A-Za-z0-9 | head -c5
-	echo
+  tr </dev/urandom -dc A-Za-z0-9 | head -c5
+  echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
-	ip64() {
-		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+  ip64() {
+    echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+  }
+  echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 install_3proxy() {
-    echo "installing 3proxy"
-    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
-    make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
-    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
-    chkconfig 3proxy on
-    cd $WORKDIR
+  echo "installing 3proxy"
+  #URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+  URL="https://github.com/z3APA3A/3proxy/archive/0.9.3.tar.gz"
+  wget -qO- $URL | bsdtar -xvf-
+  cd 3proxy-0.9.3
+  make -f Makefile.Linux
+  mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+  cp src/3proxy /usr/local/etc/3proxy/bin/
+  cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+  chmod +x /etc/init.d/3proxy
+  chkconfig 3proxy on
+  cd $WORKDIR
 }
 
 gen_3proxy() {
-    cat <<EOF
+  cat <<EOF
 daemon
-maxconn 50
+maxconn 1000
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 flush
-auth none
+auth strong
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-$(awk -F "/" '{print "auth none\n" \
+$(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
@@ -54,33 +45,58 @@ EOF
 }
 
 gen_proxy_file_for_user() {
-    cat >proxy.txt <<EOF
+  cat >proxy.txt <<EOF
 $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
 upload_proxy() {
-    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"   
+  local PASS=$(random)
+  zip --password $PASS proxy.zip proxy.txt
+  URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
+
+  echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+  echo "Download zip archive from: ${URL}"
+  echo "Password: ${PASS}"
+
 }
+
+install_jq() {
+  wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+  chmod +x ./jq
+  cp jq /usr/bin
+}
+
+upload_2file() {
+  local PASS=$(random)
+  zip --password $PASS proxy.zip proxy.txt
+  JSON=$(curl -F "file=@proxy.zip" https://file.io)
+  URL=$(echo "$JSON" | jq --raw-output '.link')
+
+  echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+  echo "Download zip archive from: ${URL}"
+  echo "Password: ${PASS}"
+}
+
 gen_data() {
-    seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
-    done
+  seq $FIRST_PORT $LAST_PORT | while read port; do
+    echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+  done
 }
 
 gen_iptables() {
-    cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+  cat <<EOF
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA})
 EOF
 }
 
 gen_ifconfig() {
-    cat <<EOF
+  cat <<EOF
 $(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
 echo "installing apps"
-yum -y install gcc net-tools bsdtar zip wget nano >/dev/null
+yum -y install gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
@@ -88,9 +104,8 @@ echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
-echo "Write internal IPv4"
-read IP
-IP4=$IP
+
+IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
@@ -119,8 +134,6 @@ bash /etc/rc.local
 
 gen_proxy_file_for_user
 
-upload_proxy
+# upload_proxy
 
-
-echo "Listening proxys"
-watch -n 5 service 3proxy start
+install_jq && upload_2file
